d6ce6d8cf18dea95484a9241d63b421ffb308e69
Khan, Imran, Vodafone Group <imran.khan18@vodafone.com>
2017-12-22 17:12:57 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Get the latest news on your terminal
+
+This is a simple bash script which fetches the latest international news headlines and corresponding URL's from [Reuters](https://uk.reuters.com/).
+
---
+++ news.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Simple breaking news aggregator.
+
+today=$(date)
+
+printf -v d "Reuters headlines as of $(date)."
+echo "$d"
+echo
+
+curl -s https://uk.reuters.com/assets/jsonWireNews |
+awk '/"url":|"headline":/' |
+cut -d'"' -f4 |
+awk '/^\// { print "\033[0mhttps://uk.reuters.com" $0; next } {print "\033[1m" $0 }' |
+#head -n 10
